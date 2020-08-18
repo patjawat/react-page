7dd87b1d984ffb250a3a4e6f395fc35df625ae52
patjawat <patjawat@gmail.com>
2020-08-18 09:06:13 +0700

    Updates

---
+++ precache-manifest.b6abde6b00b8588ab15e9e69eae5cf72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13d1f23a1cb5b18c52c56a2d5552afe",
+    "url": "/page/index.html"
+  },
+  {
+    "revision": "ad24d0ab1f175205ca1c",
+    "url": "/page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5bd93647851e2b500984",
+    "url": "/page/static/js/2.e37b2f0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/page/static/js/2.e37b2f0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad24d0ab1f175205ca1c",
+    "url": "/page/static/js/main.c008fa07.chunk.js"
+  },
+  {
+    "revision": "62def74070ae5fa191d3",
+    "url": "/page/static/js/runtime-main.88ae2c21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/page/static/media/logo.5d5d9eef.svg"
+  }
+]);
